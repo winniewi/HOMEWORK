aff10239e2845d4bc5358293c5a183cf4163811f
winnie <winniewi@icloud.com>
2015-07-11 09:01:32 +0800

    first commit

---
+++ HW/Controllers/V_ReportController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HW.Models;
+
+namespace HW.Controllers
+{
+    public class V_ReportController : Controller
+    {
+        private CustomerEntities db = new CustomerEntities();
+
+        // GET: V_Report
+        public ActionResult Index()
+        {
+            return View(db.V_Report.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HW.MvcApplication" Language="C#" %>
